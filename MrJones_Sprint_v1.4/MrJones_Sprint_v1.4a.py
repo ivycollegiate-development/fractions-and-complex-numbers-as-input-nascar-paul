a3ff61b0ea8bae794150f9abc0694e30afcbe470
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-19 05:53:39 +0000

    Creates script to prompt and display a fraction

---
+++ MrJones_Sprint_v1.4/MrJones_Sprint_v1.4a.py
@@ -0,0 +1,5 @@
+# to import the Fraction module from the fractions class, write the following code
+from fractions import Fraction
+# naming the output of the user generated input as a function that we will call a
+a = Fraction (input ("Please type in a fractional number here: (ex: 3/4): "))
+print (a)
